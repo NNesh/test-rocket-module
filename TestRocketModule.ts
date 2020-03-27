0abb973a85d37b5da37e1cf9aee21b6dec49127a
Ruslan Mullayanov <mullruslan@yandex.ru>
2020-03-27 22:52:29 +0500

    Init

---
+++ TestRocketModule.ts
@@ -0,0 +1,13 @@
+import {
+    IAppAccessors,
+    ILogger,
+    IRead // here
+} from '@rocket.chat/apps-engine/definition/accessors';
+import { App } from '@rocket.chat/apps-engine/definition/App';
+import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+
+export class TestRocketModule extends App {
+    constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
+        super(info, logger, accessors);
+    }
+}
